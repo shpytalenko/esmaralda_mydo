0718fba961003e60181a6b1bb35788a5802c8482
Pasha <shpytalenko@gmail.com>
2014-01-02 14:06:22 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Esmeralda::Application.config.secret_token = '019e524aab5c746559dbfd6c0aefd375125e2d7a1567df2b544943b2f24159712726b578d962f716272e42ab2c345339bc49966e4aceb98ff96cd8d6ee2bee3e'
